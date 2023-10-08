b205baa2036d811bc63371f7285349879c1959c9
yugyusang <yugyusang12@gmail.com>
2023-10-08 19:56:39 +0900

    배너이미지에 포크 추가

--- components/SandwichBanner.tsx
+++ components/SandwichBanner.tsx
@@ -1,7 +1,8 @@
-import { useRef, useEffect } from 'react';
+import { useRef, useEffect, useState } from 'react';
 
 const SandwichBanner = () => {
     const sandwichRef: { current: HTMLCanvasElement | null } = useRef(null);
+    const [forkPosition, setForkPosition] = useState({ x: 0, y: -100, angle: 0 });
 
     useEffect(() => {
         const context = sandwichRef.current ? sandwichRef.current.getContext('2d') : null; // 그래픽 렌더링 컨텍스트 가져오기
@@ -26,47 +27,150 @@ const SandwichBanner = () => {
                     context.scale(scale, scale);
                     context.drawImage(image, 0, 0, rect.width, rect.height);
 
+                    ///이미지 체크 그리드
+                    // 그리드의 갯수를 설정합니다.
+                    const rows = 5; // 행 수
+                    const cols = 5; // 열 수
+
+                    // 그리드의 크기를 계산합니다.
+                    const gridWidth = parseInt(canvas.style.width) / cols;
+                    const gridHeight = parseInt(canvas.style.height) / rows;
+
+                    // 결과를 저장할 배열을 생성합니다.
+                    const gridValues: boolean[][] = [];
+
+                    // 각 그리드에 대해 확인합니다.
+                    for (let i = 0; i < rows; i++) {
+                        const row: boolean[] = [];
+                        for (let j = 0; j < cols; j++) {
+                            // 각 그리드의 좌표를 계산합니다.
+                            const x = j * gridWidth;
+                            const y = i * gridHeight;
+
+                            // 해당 그리드의 이미지 데이터를 가져옵니다.
+                            const imageData = context.getImageData(x, y, gridWidth, gridHeight);
+
+                            // 이미지 데이터에 어떤 처리를 하거나 체크합니다.
+                            // 이 예제에서는 이미지 데이터가 비어있는지를 체크합니다.
+                            let isEmpty = true;
+                            for (let k = 0; k < imageData.data.length; k += 4) {
+                                if (imageData.data[k + 3] !== 0) {
+                                    isEmpty = false;
+                                    break;
+                                }
+                            }
+                            // 결과를 배열에 추가합니다.
+                            row.push(isEmpty);
+                        }
+                        gridValues.push(row);
+                    }
+/*
+                    console.log(gridValues);
+                    //이미지 그리드 육안확인용
+                    if (context) {
+                        // 이미지 그리기
+                        context.drawImage(image, 0, 0, rect.width, rect.height);
+
+                        // 그리드 그리기
+                        for (let i = 0; i <= cols; i++) {
+                            const x = i * gridWidth;
+                            context.moveTo(x, 0);
+                            context.lineTo(x, rect.height * scale);
+                        }
+
+                        for (let i = 0; i <= rows; i++) {
+                            const y = i * gridHeight;
+                            context.moveTo(0, y);
+                            context.lineTo(rect.width * scale, y);
+                        }
+
+                        // 그리드 색상 채우기
+                        for (let i = 0; i <= cols; i++) {
+                            for (let j = 0; j <= rows; j++) {
+                                if (gridValues[j] && gridValues[j][i] === false) {
+                                    const x = i * gridWidth;
+                                    const y = j * gridHeight;
+                                    context.fillStyle = 'rgba(2, 255, 255, 0.5)';
+                                    context.fillRect(x, y, gridWidth, gridHeight);
+                                }
+                            }
+                        }
+                        context.strokeStyle = 'rgba(0, 0, 0, 0.5)';
+                        context.stroke();
+                    }*/
+
                     const clickListener = (event) => {
                         const x = event.clientX - rect.left;
                         const y = event.clientY - rect.top;
 
-                        //context.clearRect(x, y, 10 / scale, 10 / scale);
-                        context.save();
-                        context.beginPath();
-                        context.arc(x, y, 30, 0, Math.PI * 2);
-                        context.clip();
-                        context.clearRect(0, 0, rect.width * scale, rect.height * scale);
-                        context.restore();
-                        console.log('지워짐');
-                    };
+                        // 클릭한 위치를 그리드의 좌표로 변환
+                        const gridX = Math.floor(x / gridWidth);
+                        const gridY = Math.floor(y / gridHeight);
+                        // 현재 위치와 주변 위치의 값 확인
+                        const currentCell = gridValues[gridY] && gridValues[gridY][gridX];
+                        const leftCell = gridValues[gridY] && gridValues[gridY][gridX - 1];
+                        const rightCell = gridValues[gridY] && gridValues[gridY][gridX + 1];
+                        const upCell = gridValues[gridY - 1] && gridValues[gridY - 1][gridX];
+                        const downCell = gridValues[gridY + 1] && gridValues[gridY + 1][gridX];
 
-                    const moveListener = (event) => {
+                        // 현재 위치가 false이면서 상하좌우 중 하나가 true일 때만 동작
+                        if (currentCell === false && (leftCell || rightCell || upCell || downCell || !gridValues[gridY] || gridX === 0 || gridX === cols - 1)) {
+                            context.save();
+                            context.beginPath();
+                            context.arc(x, y, 30, 0, Math.PI * 2);
+                            context.clip();
+                            context.clearRect(0, 0, rect.width * scale, rect.height * scale);
+                            context.restore();
+                            console.log('지워짐');
+
+                            // 클릭 후에도 해당 위치의 이미지 데이터 확인
+                            const imageData = context.getImageData(gridX * gridWidth, gridY * gridHeight, gridWidth, gridHeight);
+
+                            let isEmpty = true;
+                            for (let k = 0; k < imageData.data.length; k += 4) {
+                                if (imageData.data[k + 3] !== 0) {
+                                    isEmpty = false;
+                                    break;
+                                }
+                            }
+
+                            // 결과를 배열에 업데이트
+                            gridValues[gridY][gridX] = isEmpty;
+                            console.log(gridValues);
+                        }
+                    };
+                    canvas.addEventListener('mouseup', clickListener);
+
+                    const mouseMoveListener = (event) => {
                         const x = event.clientX - rect.left;
                         const y = event.clientY - rect.top;
 
-                        const dx = x - rect.width / 2;
-                        const dy = y - rect.height / 2;
-                        const distance = Math.sqrt(dx * dx + dy * dy);
-                        const maxDistance = Math.sqrt(Math.pow(rect.width / 2, 2) + Math.pow(rect.height / 2, 2));
-                        const scale = 1 + (distance / maxDistance) * 0.1;
-                        canvas.style.transform = `scale(${scale})`;
-                        canvas.style.transition = 'transform 0.5s ease-out';
+                        const gridX = Math.floor(x / gridWidth);
+                        const gridY = Math.floor(y / gridHeight);
 
-                    }
+                        const currentCell = gridValues[gridY] && gridValues[gridY][gridX];
+                        const leftCell = gridValues[gridY] && gridValues[gridY][gridX - 1];
+                        const rightCell = gridValues[gridY] && gridValues[gridY][gridX + 1];
+                        const upCell = gridValues[gridY - 1] && gridValues[gridY - 1][gridX];
+                        const downCell = gridValues[gridY + 1] && gridValues[gridY + 1][gridX];
 
-                    const outListener = () => {
-                        canvas.style.transform = `scale(1, 1)`;
-                        canvas.style.transition = 'transform 0.5s ease-out';
-                    }
+                        if (currentCell === false && (leftCell || rightCell || upCell || downCell || !gridValues[gridY] || gridX === 0 || gridX === cols - 1)) {
+                            const centerX = rect.width * scale / 2; // 캔버스의 중앙 x 좌표
+                            const centerY = rect.height * scale / 2; // 캔버스의 중앙 y 좌표
+                            const angle = Math.atan2(y - centerY, x - centerX);
 
-                    canvas.addEventListener('mouseup', clickListener);
-                    canvas.addEventListener('mousemove', moveListener);
-                    canvas.addEventListener('mouseout', outListener);
+                            setForkPosition({ x, y, angle });
+                            console.log(forkPosition);
+                        }else{
+                            setForkPosition({ x:0, y:-100, angle:0 });
+                        }
+                    };
+
+                    canvas.addEventListener('mousemove', mouseMoveListener);
 
                     return () => {
                         canvas.removeEventListener('mouseup', clickListener);
-                        canvas.removeEventListener('mousemove', moveListener);
-                        canvas.removeEventListener('mouseout', outListener);
+                        canvas.removeEventListener('mousemove', mouseMoveListener);
                     };
 
                 }
@@ -77,9 +181,24 @@ const SandwichBanner = () => {
     }, []);
 
     return (
-        <canvas ref={sandwichRef} className='mx-auto w-80 h-80 animate-customBounce'>
-            <img src="/images/front_banner.png" alt="subway" className='mx-auto w-80' />
-        </canvas>
+        <div style={{ position: 'relative', width: '100%', height: '100%' }}>
+            <canvas ref={sandwichRef} className='mx-auto w-80 h-80'></canvas>
+            {forkPosition && (
+                <img
+                    src='/images/fork.png'
+                    alt='fork'
+                    style={{
+                        position: 'absolute',
+                        top: forkPosition.y,
+                        left: forkPosition.x +100,
+                        pointerEvents: 'none',
+                        transform: `rotate(${forkPosition.angle + 180}rad)`,
+                        transformOrigin: 'top center', // 회전 중심점을 이미지 중앙으로 설정
+                        transition: 'top 1s, left 1s, transform 1s'
+                    }}
+                />
+            )}
+        </div>
     );
 };
 
