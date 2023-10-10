f1823d2fb303b6a0520bc265bc7f57235d1d3d8a
yugyusang <yugyusang12@gmail.com>
2023-10-10 18:48:45 +0900

    menus페이지 작성중

--- components/MenusSelectorGridItem.tsx
+++ components/MenusSelectorGridItem.tsx
@@ -1,11 +1,12 @@
-const MenusSelectorGridItem = ({menuName,src}) => {
+import Link from "next/link";
+const MenusSelectorGridItem = ({ menuName, src, href }) => {
     return (
-        <div className="col-span-1">
+        <Link href={href} className="col-span-1">
             <div className=' overflow-hidden relative rounded-md  aspect-square'>
                 <img src={src} className='relative object-cover scale-[2.7] origin-[85%_40%]'></img>
             </div>
             <div className='text-left text-xs align-top leading-3'>{menuName}</div>
-        </div>
+        </Link>
     );
 };
 
--- pages/Menus.tsx
+++ pages/Menus.tsx
@@ -1,43 +1,47 @@
 import React, { useState } from 'react';
 import MenusSelectorGridItem from '../components/MenusSelectorGridItem';
+import Link from 'next/link';
 
 const Menus = () => {
-    const menuArray: { name: string, value: string }[] = [
-        { name: '0', value: '1' },
-        { name: '2', value: '1' },
-        { name: '4', value: '1' },
-        { name: '5', value: '1' },
-        { name: '1', value: '4' },
-        { name: '3', value: '3' },
+    const arrayTemplate: { name: string, favorit: string, recipes: string, matches: string } = {
+        name: '메뉴 이름', favorit: '좋아요 수', recipes: '레시피 수', matches: '더 하면 좋은 재료'
+    }
+    const menuArray: { name: string, favorit: string, recipes: string, matches?: Array<any> }[] = [
+        { name: '베지', favorit: '1', recipes: '1' },
+        { name: '서브웨이클럽', favorit: '2', recipes: '1' },
+        { name: '스파이시 쉬림프', favorit: '3', recipes: '1' },
+        { name: '스파이시 바베큐', favorit: '1', recipes: '1' },
+        { name: '스파이시 이탈리안', favorit: '4', recipes: '1' },
+        { name: '스테이크 & 치즈', favorit: '3', recipes: '1' },
     ];
 
     const [menuType, setMenuType] = useState(0);
     const menuSelectorArray: { menuName: string, src: string, type?: number }[] = [
-        { menuName: '로스티드 치킨', src: '/images/sandwich_menu/Roasted-Chicken_20211231095032718.png', type:2},
-        { menuName: '로티세리 바베큐', src: '/images/sandwich_menu/Rotisserie-Barbecue-Chicken_20211231023137878.png', type:2},
-        { menuName: '베지', src: '/images/sandwich_menu/Veggie-Delite_20211231095658375.png', type:2},
-        { menuName: '서브웨이클럽', src: '/images/sandwich_menu/Subway-Club™_20211231095518589.png', type:2},
-        { menuName: '스파이시 쉬림프', src: '/images/sandwich_menu/치킨슬라이스샌드위치_20220804012537491.png', type:3},
-        { menuName: '스파이시 바베큐', src: '/images/sandwich_menu/스파이시바비큐_정면_20221031041334845.png', type:4},
-        { menuName: '스파이시 이탈리안', src: '/images/sandwich_menu/spicy_italian_20211231095435532.png', type:3},
-        { menuName: '스테이크 & 치즈', src: '/images/sandwich_menu/Steak-&-Cheese_20211231095455613.png', type:3},
-        { menuName: '쉬림프', src: '/images/sandwich_menu/Shrimp_20211231095411189.png', type:3},
-        { menuName: '이탈리안 B.M.T', src: '/images/sandwich_menu/Italian_B.M.T_20211231094910899.png', type:1},
-        { menuName: '에그마요', src: '/images/sandwich_menu/Egg-Mayo_20211231094817112.png', type:1},
-        { menuName: '참치', src: '/images/sandwich_menu/Tuna_20211231095535268.png', type:1},
-        { menuName: '치킨 베이컨 아보카도', src: '/images/sandwich_menu/치킨베이컨아보카도샌드위치_20220804012954461.png', type:2},
-        { menuName: '치킨 슬라이스', src: '/images/sandwich_menu/치킨슬라이스샌드위치_20220804012537491.png', type:2},
-        { menuName: '치킨 데리야끼', src: '/images/sandwich_menu/Chicken-Teriyaki_20211231094803381.png', type:3},
-        { menuName: '풀드포크', src: '/images/sandwich_menu/Pulled-Pork+cheese_20211231095012512.png', type:3},
-        { menuName: '햄', src: '/images/sandwich_menu/Ham_20211231094833168.png', type:1},
-        { menuName: 'B.L.T', src: '/images/sandwich_menu/B.L.T_20211231094744175.png', type:1},
-        { menuName: 'K-bbq', src: '/images/sandwich_menu/K-BBQ_20211231094930225.png', type:3},
+        { menuName: '로스티드 치킨', src: '/images/sandwich_menu/Roasted-Chicken_20211231095032718.png', type: 2 },
+        { menuName: '로티세리 바베큐', src: '/images/sandwich_menu/Rotisserie-Barbecue-Chicken_20211231023137878.png', type: 2 },
+        { menuName: '베지', src: '/images/sandwich_menu/Veggie-Delite_20211231095658375.png', type: 2 },
+        { menuName: '서브웨이클럽', src: '/images/sandwich_menu/Subway-Club™_20211231095518589.png', type: 2 },
+        { menuName: '스파이시 쉬림프', src: '/images/sandwich_menu/치킨슬라이스샌드위치_20220804012537491.png', type: 3 },
+        { menuName: '스파이시 바베큐', src: '/images/sandwich_menu/스파이시바비큐_정면_20221031041334845.png', type: 4 },
+        { menuName: '스파이시 이탈리안', src: '/images/sandwich_menu/spicy_italian_20211231095435532.png', type: 3 },
+        { menuName: '스테이크 & 치즈', src: '/images/sandwich_menu/Steak-&-Cheese_20211231095455613.png', type: 3 },
+        { menuName: '쉬림프', src: '/images/sandwich_menu/Shrimp_20211231095411189.png', type: 3 },
+        { menuName: '이탈리안 B.M.T', src: '/images/sandwich_menu/Italian_B.M.T_20211231094910899.png', type: 1 },
+        { menuName: '에그마요', src: '/images/sandwich_menu/Egg-Mayo_20211231094817112.png', type: 1 },
+        { menuName: '참치', src: '/images/sandwich_menu/Tuna_20211231095535268.png', type: 1 },
+        { menuName: '치킨 베이컨 아보카도', src: '/images/sandwich_menu/치킨베이컨아보카도샌드위치_20220804012954461.png', type: 2 },
+        { menuName: '치킨 슬라이스', src: '/images/sandwich_menu/치킨슬라이스샌드위치_20220804012537491.png', type: 2 },
+        { menuName: '치킨 데리야끼', src: '/images/sandwich_menu/Chicken-Teriyaki_20211231094803381.png', type: 3 },
+        { menuName: '풀드포크', src: '/images/sandwich_menu/Pulled-Pork+cheese_20211231095012512.png', type: 3 },
+        { menuName: '햄', src: '/images/sandwich_menu/Ham_20211231094833168.png', type: 1 },
+        { menuName: 'B.L.T', src: '/images/sandwich_menu/B.L.T_20211231094744175.png', type: 1 },
+        { menuName: 'K-bbq', src: '/images/sandwich_menu/K-BBQ_20211231094930225.png', type: 3 },
     ]
-    const [searchQuery,setSearchQuery]=useState('');
-    const queryChange = (e:React.ChangeEvent<HTMLInputElement>) =>{
+    const [searchQuery, setSearchQuery] = useState('');
+    const queryChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         setSearchQuery(e.target.value);
     }
-    const searchResult = menuSelectorArray.filter((item)=>item.menuName.includes(searchQuery))
+    const searchResult = menuSelectorArray.filter((item) => item.menuName.includes(searchQuery))
 
     return (
         <main className='w-full max-w-screen-xl mx-auto'>
@@ -45,12 +49,12 @@ const Menus = () => {
             <div className="grid grid-cols-6 gap-2 w-[66rem] pt-4">
                 <div className="col-span-6 border bg-white">
                     {/*메뉴 간단정보*/}
-                    as23df+{searchQuery}
+                    {searchQuery}
                 </div>
 
                 <div className="col-span-2 border bg-white p-2">
                     {/*메뉴 선택기 */}
-                    <input type="text" className='border placeholder:text-gray-400 focus:ring-0 w-full p-1' onChange={queryChange}/>
+                    <input type="text" className='border placeholder:text-gray-400 focus:ring-0 w-full p-1' onChange={queryChange} />
                     <div className="flex w-full pt-2">
                         <button className={`border rounded-l py-2 w-full text-xs ${menuType === 0 ? 'bg-blue-500 text-white' : ''}`} onClick={() => setMenuType(0)}>all</button>
                         <button className={`border-y py-2 w-full text-xs ${menuType === 1 ? 'bg-blue-500 text-white' : ''}`} onClick={() => setMenuType(1)}>클래식</button>
@@ -61,34 +65,36 @@ const Menus = () => {
 
                     {menuType === 0 &&
                         <div className='grid grid-cols-5 pt-2  gap-2 relative'>
-                            {searchResult.map((index) => (<MenusSelectorGridItem menuName={index.menuName} src={index.src} key={index.menuName}/>))}
+                            {searchResult.map((index) => (<MenusSelectorGridItem menuName={index.menuName} src={index.src} key={index.menuName} href={''}/>))}
                         </div>
                     }{menuType === 1 &&
                         <div className='grid grid-cols-5 pt-2  gap-2 relative'>
-                            {searchResult.map((index) => (index.type === 1 && <MenusSelectorGridItem menuName={index.menuName} src={index.src} />))}
+                            {searchResult.map((index) => (index.type === 1 && <MenusSelectorGridItem menuName={index.menuName} src={index.src} href={''}/>))}
                         </div>
                     }{menuType === 2 &&
                         <div className='grid grid-cols-5 pt-2  gap-2 relative'>
-                            {searchResult.map((index) => (index.type === 2 && <MenusSelectorGridItem menuName={index.menuName} src={index.src} />))}
+                            {searchResult.map((index) => (index.type === 2 && <MenusSelectorGridItem menuName={index.menuName} src={index.src} href={''}/>))}
                         </div>
                     }{menuType === 3 &&
                         <div className='grid grid-cols-5 pt-2  gap-2 relative'>
-                            {searchResult.map((index) => (index.type === 3 && <MenusSelectorGridItem menuName={index.menuName} src={index.src} />))}
+                            {searchResult.map((index) => (index.type === 3 && <MenusSelectorGridItem menuName={index.menuName} src={index.src} href={''}/>))}
                         </div>
                     }{menuType === 4 &&
                         <div className='grid grid-cols-5 pt-2  gap-2 relative'>
-                            {searchResult.map((index) => (index.type === 4 && <MenusSelectorGridItem menuName={index.menuName} src={index.src} />))}
+                            {searchResult.map((index) => (index.type === 4 && <MenusSelectorGridItem menuName={index.menuName} src={index.src} href={''}/>))}
                         </div>
                     }
                 </div>
-
-                <div className="col-span-4 border bg-white">
-                    {/*메뉴 순위 */
-                        menuArray?.map((item, index) => (
-                            <div className={`relative w-96 h-12 flex justify-center items-center`} key={index}>
-                                <p className="absolute left-4">{index + 1}</p>{item.name} + {item.value}
-                            </div>
-                        ))}
+                {/*메뉴 순위 */}
+                <div className="col-span-4 border bg-white relative w-full p-2 divide-y text-sm">
+                    <div className='py-1'>
+                        <span className="inline-block w-[10%] text-center">순위</span><span className="inline-block w-[30%]">{arrayTemplate.name}</span><span className="inline-block w-[15%] text-center">{arrayTemplate.favorit}</span><span className="inline-block w-[15%] text-center">{arrayTemplate.recipes}</span><span className="inline-block w-[30%] text-center">{arrayTemplate.matches}</span>
+                    </div>
+                    {menuArray?.map((item, index) => (
+                        <Link href={'/'} key={index} className='block py-3'>
+                            <span className="inline-block w-[10%] text-center">{index + 1}</span><span className="inline-block w-[30%]">{item.name}</span><span className="inline-block w-[15%] text-center">{item.favorit}</span><span className="inline-block w-[15%] text-center">{item.recipes}</span><span className="inline-block w-[30%] text-center">{item.matches}</span>
+                        </Link>
+                    ))}
                 </div>
             </div>
         </main>
