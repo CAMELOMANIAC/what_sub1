21cd4ba8fd8d624246ba49591a40e2f58c50bfca
yugyusang <yugyusang12@gmail.com>
2023-11-22 23:55:40 +0900

    index페이지에서 Recipe페이지로 검색 전송기능 구현

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,7 @@ import FrontPopularRecipe from '../components/FrontPopularRecipe'
 import SandwichBanner from '../components/SandwichBanner'
 import { FaSearch } from 'react-icons/fa';
 import { useEffect, useRef, useState } from 'react';
+import { useRouter } from 'next/router';
 
 const IndexPage = () => {
   const test: { name: string, value: string }[] = [
@@ -14,18 +15,40 @@ const IndexPage = () => {
     { name: '3', value: '3' },
   ];
 
+  const [searchQuery,setSearchQuery] = useState<string>('');
   const [isFocus, setFocus] = useState<boolean>(false);
   const inputRef = useRef<HTMLInputElement>(null);
+
   useEffect(() => {
     if (isFocus){
       inputRef.current && inputRef.current.focus();
-      console.log(inputRef.current);
-      console.log( document.activeElement);
+    }
+  }, [isFocus]);
+
+  useEffect(()=>{
+    const focusOutHandler = (e) => {
+      if (e.target !== inputRef.current && searchQuery === '') {
+        setFocus(false);
+        console.log('포커스해제')
+      }
     }
     
-  }, [isFocus]);
-  
+    addEventListener("mousedown",focusOutHandler)
+    return()=>{
+      removeEventListener("mousedown",focusOutHandler)
+    }
+  },[searchQuery])
 
+  const router = useRouter();
+  const searchEnterHandler = (e) => {
+    if (e.key === 'Enter'){
+      queryPushHandler();
+    }
+  }
+  const queryPushHandler = () => {
+    router.push(`/Recipes?query=${searchQuery}`)
+  }
+  
   return (
     <main className=' w-full max-w-screen-xl mx-auto'>
       <div className='flex flex-col items-center justify-center mx-auto mb-4 rounded-full w-[100%]'>
@@ -33,14 +56,14 @@ const IndexPage = () => {
         {
           <div className='w-auto bg-white border-[12px] border-green-600 rounded-full px-6 py-2 flex flex-row justify-center items-center'>
             <span className='flex justify-center items-center w-[70px] text-white text-4xl font-extrabold font-[seoul-metro] rounded-full bg-green-600 aspect-square mr-6'>
-              <button>
+              <button onClick={queryPushHandler}>
                 <FaSearch />
               </button>
             </span>
 
             <div className='flex flex-col flex-nowrap justify-center items-center mr-6 my-2 text-5xl' onClick={() => setFocus(true)}>
               {isFocus ?
-                <input className='w-1/2' type='text' ref={inputRef}></input> :
+                <input className='p-5 text-lg text-center' type='text' ref={inputRef} onChange={(e)=>setSearchQuery(e.currentTarget.value)} onKeyDown={searchEnterHandler}></input> :
                 <Logo />
               }
               <div className='font-[seoul-metro] text-gray-600 text-lg'>넌 뭐먹어?</div>
