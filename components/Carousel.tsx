7b5d64e37c9e21f0064efb2aec89afe71cf558a2
yugyusang <yugyusang12@gmail.com>
2023-11-24 23:52:13 +0900

    캐러셀 자연스럽게 움직이도록 수정

--- components/Carousel.tsx
+++ components/Carousel.tsx
@@ -1,4 +1,4 @@
-import React, { RefObject, createRef, useCallback, useEffect, useRef, useState } from 'react';
+import React, { RefObject, createRef, useEffect, useRef, useState } from 'react';
 import { styled } from 'styled-components';
 import { recipeContextType } from '../interfaces/AddRecipe';
 import Card from './Card';
@@ -20,7 +20,28 @@ type PropsType = {
 const Carousel = ({ recipeData }: PropsType) => {
     const [recipeArray] = useState<recipeContextType[]>([...recipeData, ...recipeData]);
     const crouselRef = useRef<HTMLInputElement>(null);
-    const [isMove] = useState<boolean>(true)
+    const [isMoving, setIsMoving] = useState<boolean>(true);
+
+    useEffect(() => {
+        let animationId;
+
+        const carouselMove = () => {
+            // 애니메이션 코드
+            if (isMoving === false) {
+                cancelAnimationFrame(animationId);
+            } else {
+                animationId = requestAnimationFrame(carouselMove);
+                if (crouselRef.current) {
+                    crouselRef.current.scrollLeft += 0.5;
+                }
+            }
+        };
+        animationId = requestAnimationFrame(carouselMove);
+
+        return ()=>{
+            cancelAnimationFrame(animationId);
+        }
+    }, [isMoving])
 
     //옵저버 객체 생성 및 탐지 요소 등록
     const recipeRefArray = useRef<RefObject<HTMLDivElement>[]>(recipeArray.map(() => createRef()));
@@ -33,25 +54,14 @@ const Carousel = ({ recipeData }: PropsType) => {
                 }
             }
         });
-
-        let timer;
-        if (isMove) {
-            timer = setInterval(() => setCurrentCarousel(prev => prev + 1), 5000)
-        }
-
-        return () => {
-            observer.disconnect();
-            clearInterval(timer)
-        };
     }, [])
-
 
     //요소가 감지되면 실행될 콜백
     const callback = (entries) => {
         entries.forEach((entry, _index) => {
             if (entry.isIntersecting === false && entry.target === recipeRefArray.current[recipeRefArray.current.length / 2 - 1].current) {
                 if (crouselRef.current) {
-                    crouselRef.current.scrollLeft = 12;//레시피 사이 여백을 감안해서 스크롤 위치를 다시 지정
+                    crouselRef.current.scrollLeft = 12;//레시피 사이 여백을 감안해서 스크롤 위치를 처음으로 되돌림
                     setCurrentCarousel(0)
                 }
             }
@@ -65,6 +75,17 @@ const Carousel = ({ recipeData }: PropsType) => {
         recipeRefArray.current[currentCarousel].current!.scrollIntoView({ behavior: 'smooth', inline: 'start' })
     }, [currentCarousel])
 
+    const prevCarousel = () => {
+        setCurrentCarousel(prev => (prev > 1) ? prev - 1 : recipeRefArray.current.length/2 - 1)
+        setIsMoving(false)
+        setTimeout(() => {setIsMoving(true)},1000)
+    }
+    const nextCarousel = () => {
+        setCurrentCarousel(prev => (prev < recipeRefArray.current.length - 1) ? prev + 1 : prev)
+        setIsMoving(false)
+        setTimeout(() => {setIsMoving(true)},1000)
+    }
+
     return (
         <>
             <CarouselContainer className='max-w-[100vw] pt-12 p-5 flex flex-row overflow-scroll gap-2' ref={crouselRef}>
@@ -72,8 +93,9 @@ const Carousel = ({ recipeData }: PropsType) => {
                     <Card key={index} recipe={recipe} ref={recipeRefArray.current[index]} id={index}></Card>
                 ))}
             </CarouselContainer>
-            <button onClick={() => setCurrentCarousel(prev => (prev > 1) ? prev - 1 : 0)}>이전</button>
-            <button onClick={() => setCurrentCarousel(prev => (prev < recipeRefArray.current.length - 1) ? prev + 1 : 0)}>다음</button>
+            <button onClick={()=>setIsMoving(prev => prev === true ? false : true)}>정지</button>
+            <button onClick={prevCarousel}>이전</button>
+            <button onClick={nextCarousel}>다음</button>
         </>
     );
 };
