b4123987e3b4371d33ee82f5959433bef270971e
yugyusang <yugyusang12@gmail.com>
2024-01-18 17:39:48 +0900

    api변경후 Recipes페이지 검색이 안되던 문제 수정

--- pages/Recipes.tsx
+++ pages/Recipes.tsx
@@ -61,14 +61,21 @@ const Recipes = ({ recipeData, menuData }: propsType) => {
     const bannerRef = useRef<HTMLDivElement>(null);
     const mainRef = useRef<HTMLDivElement>(null);
     const [recipes, setRecipes] = useState<recipeType[]>([]);
-    const [param, setParam] = useState<string | string[]>(router.query.param);
-    const [query, setQuery] = useState<string | string[]>(router.query.query);
     const filter = useSelector((state: RootState) => state.page.FILTER_ARRAY);
     const [page, setPage] = useState<number>(0);
     const [loading, setLoading] = useState<number>(loadingState.fullfiled);
     const disptach = useDispatch();
 
-    //새로고침시 정보 불러오는용
+    //배너랑 글로벌네비 높이 여백계산(router.query가 변경되면 bannerRef의 높이가 변경되므로 의존성배열에 추가함)
+    useEffect(() => {
+        if (bannerRef.current) {
+            if (mainRef.current) {
+                mainRef.current.style.marginTop = bannerRef.current.offsetHeight + 50 + 'px';
+            }
+        }
+    }, [bannerRef.current && bannerRef.current.offsetHeight]);
+
+    //새로고침시 좋아요 정보 불러오는용
     useEffect(() => {
         if (getCookieValue('user').length > 0) {//로그인 정보가 있을경우
             loadRecipeLike().then(data => {//레시피 좋아요 정보 가져와서 전역 상태값에 저장
@@ -81,74 +88,67 @@ const Recipes = ({ recipeData, menuData }: propsType) => {
         }
     }, [])
 
-    //배너랑 글로벌네비 높이 여백계산(router.query가 변경되면 bannerRef의 높이가 변경되므로 의존성배열에 추가함)
-    useEffect(() => {
-        if (bannerRef.current) {
-            if (mainRef.current) {
-                mainRef.current.style.marginTop = bannerRef.current.offsetHeight + 50 + 'px';
+    const searchHandler = (limit: number) => {
+        if (router.query) {
+            if (router.query.query) {//쿼리로 검색
+                if (typeof router.query.query === 'string') {
+                    getRecipes(router.query.query, page, limit, filterQuery)
+                }
+            } else if (router.query.param) {//파라메터로 검색
+                if (typeof router.query.param === 'string') {
+                    getRecipes(router.query.param, page, limit-1, filterQuery)
+                }
+            } else {
+                getRecipes('', page, limit, filterQuery)
             }
         }
-    }, [bannerRef.current && bannerRef.current.offsetHeight]);
-
-    const filterQuery = filter.join('&filter=')
-    //검색 핸들러
-    const searchHandler = (loadCount) => {
-        if (Object.keys(router.query).length !== 0) {
-            if (query) {//쿼리 검색일때
-                console.log('쿼리검색일때')
-                
-            } else if (query === '') {//쿼리검색이지만 값을 안줄때
-                console.log('//쿼리검색이지만 값을 안줄때')
-                getRecipes('', page, loadCount, filterQuery)
-            }
-            if (param) {//샌드위치 파라메터로 검색중일때
-                console.log('//샌드위치 파라메터로 검색중일때')
-                getRecipes(String(param), page, loadCount - 1, ['메뉴이름'])
-            }
-        } else {//검색어가 없을때
-            getRecipes('', page, loadCount, filterQuery)
-        }
-
     }
 
-    //보여줄 레시피불러오기
+    // 라우팅-> 초기화-> 파라메터로 api통신-> 스크롤시 추가 api통신 
+    const filterQuery = filter.join('&filter=')
+    //페이지 로딩후(라우팅 후) 보여줄 레시피 통신
     useEffect(() => {
-        //query 쿼리스트링은 검색어
-        setQuery(router.query.query)
-        //param 쿼리스트링은 배너 간단 정보
-        setParam(router.query.param)
-        //레시피 초기화(체음에는 12개 레시피를 기본 출력)
-        searchHandler(9)
-    }, [query, param, router]);
+        setPage(0);
+        setRecipes([])//레시피 배열을 비우기
+    }, [router]);
+    
+    useEffect(() => {
+        if (page === 0 && recipes.length === 0) {
+            searchHandler(9);
+        }
+    }, [page, recipes]);
 
     //recipe카드 통신함수
-    const getRecipes = (query = '', offset = 0, limit = 3, filter = filterQuery) => {
+    const getRecipes = async (query = '', offset = 0, limit = 3, filter = filterQuery) => {
         //레시피 로딩 상태
         setLoading(loadingState.pending)
         //클라이언트에서 서버로 값을 보낼때 한글은 인코딩해야함
-        //node.js서버에서는 쿼리값이 자동으로 디코딩되서 디코딩함수안써도됨
-        fetch(query !== '' ? '/api/recipes/' + encodeURIComponent(query) + `?offset=${offset}&limit=${limit}&filter=${filter}`
-            : `/api/recipes?offset=${offset}&limit=${limit}&filter=${filter}`)
-            .then(response => {
+        //node.js서버에서는 쿼리값이 자동으로 디코딩되서 디코딩 함수안써도됨
+        const fetchRecipes = async () => {
+            try {
+                const response = await fetch(query !== '' ? '/api/recipes/' + encodeURIComponent(query) + `?offset=${offset}&limit=${limit}&filter=${filter}`
+                    : `/api/recipes?offset=${offset}&limit=${limit}&filter=${filter}`)
+
                 if (response.status === 200) {
                     setLoading(loadingState.fullfiled);
-                    return response.json();
-                }
-                if (response.status === 204) {
-                    setLoading(loadingState.end);
-                    return;
-                }
-                if (response.status === 400) {
-                    setLoading(loadingState.error);
-                    return;
-                }
-            })
-            .then(data => {
-                if (data) {
+
+                    const data = await response.json();
                     setRecipes(prev => [...prev, ...data]);
                     setPage(prev => prev + data.length)
                 }
-            })
+                if (response.status === 204) {
+                    setLoading(loadingState.end);
+                    throw response
+                }
+                if (response.status === 400) {
+                    setLoading(loadingState.error);
+                    throw response
+                }
+            } catch (err) {
+                console.log(err)
+            }
+        }
+        await fetchRecipes();
     }
 
     // 타겟 요소 선택
@@ -161,7 +161,7 @@ const Recipes = ({ recipeData, menuData }: propsType) => {
             entries.forEach(entry => {
                 // 타겟 요소가 화면에 보이는 경우
                 if (entry.isIntersecting) {
-                    searchHandler(3);
+                    searchHandler(3)
                     lastRecipeRef.current && observer.unobserve(lastRecipeRef.current);
                 }
             });
@@ -190,7 +190,7 @@ const Recipes = ({ recipeData, menuData }: propsType) => {
             <RecipesBanner ref={bannerRef} recipeData={recipeData} menuData={menuData} />
             <main className={'w-full max-w-screen-lg mx-auto pt-2'} ref={mainRef}>
                 <div className='grid grid-cols-6 grid-flow-row gap-2 min-w-[1024px]'>
-                    {query !== '' && param && <EmptyCard></EmptyCard>}
+                    {recipes.length === 0 && <EmptyCard></EmptyCard>}
                     {recipes.map((recipe, index) => (
                         <Card key={index} recipe={recipe} ref={lastRecipeRef}></Card>
                     ))}
