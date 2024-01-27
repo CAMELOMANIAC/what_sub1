cb530f1721f74c42da4994bddd9795f9d15e3b39
yugyusang <yugyusang12@gmail.com>
2024-01-27 21:17:09 +0900

    회원가입시 인증메일 전송을 위한 nodemailer설치 및 설정완료

---
+++ lib/mail.ts
@@ -0,0 +1,25 @@
+import nodemailer from 'nodemailer';
+
+const transporter = nodemailer.createTransport({
+    service: 'Gmail',  // 메일 서비스 제공자
+    auth: {
+        user: process.env.MAIL_ID,  // 사용자의 이메일 주소
+        pass: process.env.MAIL_PASSWORD  // 사용자의 이메일 비밀번호
+    }
+});
+
+type mailOptionsType = {
+    from: string, // 발신자 이메일 주소
+    to: string, // 수신자 이메일 주소
+    subject: string, // 메일 제목
+    text: string, // 텍스트 본문
+    html?: string //html 태그지원하는 이메일 제공자는 html요소로 보여줄 수 도있음
+}
+
+export const sendMail = async (mailOptions:mailOptionsType) => {
+    try {
+        await transporter.sendMail(mailOptions);
+    } catch (error) {
+        return new Error(error)
+    }
+}
--- utils/publicFunction.ts
+++ utils/publicFunction.ts
@@ -34,4 +34,7 @@ export const getCookieValue = (key) => {
     return result;
 }
 
-
+export const validateEmail = (email) => {
+    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;// eslint-disable-line no-useless-escape
+    return re.test(String(email).toLowerCase());
+}
