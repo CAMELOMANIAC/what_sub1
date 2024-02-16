097e87fddfee00d395b7d97f32057fec498a689f
yugyusang <yugyusang12@gmail.com>
2024-02-16 23:44:12 +0900

    fetch의 캐시옵션을 사용하도록 수정 및 추천태그가 정상적으로 나오지 않던 문제 수정

--- components/ingredientSection/RecipeNameSection.tsx
+++ components/ingredientSection/RecipeNameSection.tsx
@@ -42,12 +42,12 @@ const RecipeNameSection = forwardRef<HTMLDivElement, propsType>(({ prop }, ref) 
     const tagSearch = async ({ tag, param }: { tag: string; param?: never } | { tag?: never; param: string }) => {
         try {
             const result = await fetch(tag ? `/api/recipes/tag?tag=${encodeURIComponent(tag)}`
-                : param ? `/api/recipes/tag?param=${encodeURIComponent(param)}` : '')
+                : param ? `/api/recipes/tag?param=${encodeURIComponent(param)}` : '', { cache: 'force-cache' })//태그는 중요하지 않으므로 브라우저 캐시를 사용
             if (result.status === 200) {
                 const data = await result.json();
                 setTagData(data.map(item => item.tag_name));
             } else if (result.status === 204) {
-                setTagData([]);
+                return setTagData([]);
             } else {
                 throw result;
             }
@@ -62,13 +62,14 @@ const RecipeNameSection = forwardRef<HTMLDivElement, propsType>(({ prop }, ref) 
             tagSearch({ param: param })
         }
     }, [])
-    
-    const throttleGetTag = useCallback(throttle((tagQuery:string) => {
+
+    //태그 검색 스로틀링(tagInput이 변경될때마다 0.7초마다 검색)
+    const throttleGetTag = useCallback(throttle((tagQuery: string) => {//tagInput이 상태값이므로 할당된 함수가 새로 생성되므로 useCallback으로 변하지 않게 해줌
         if (tagQuery.length > 1) {
             //태그검색
             tagSearch({ tag: tagQuery })
         }
-    }, 800), []);
+    }, 1000), []);
 
     useEffect(() => {
         throttleGetTag(tagInput);
@@ -98,7 +99,7 @@ const RecipeNameSection = forwardRef<HTMLDivElement, propsType>(({ prop }, ref) 
                 </div>
                 <div className='p-2'>
                     추천태그:
-                    {!tagData && <StyleTag onClick={addInputHandler}>+{tagInput}</StyleTag>}
+                    {!tagData.find(item => item === tagInput) && tagInput.length > 0 && <StyleTag onClick={addInputHandler}>+{tagInput}</StyleTag>}
                     {tagData && tagData.map(item => <StyleTag key={item} onClick={() => addTagHandler(item)}>+{item}</StyleTag>)}
                 </div>
             </div>
