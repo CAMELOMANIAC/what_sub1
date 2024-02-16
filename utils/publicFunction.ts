6101c16e8fe96b4f659655d91ae0ed86d9466737
yugyusang <yugyusang12@gmail.com>
2024-02-16 23:42:09 +0900

    공용함수 타입어노테이션 추가

--- utils/publicFunction.ts
+++ utils/publicFunction.ts
@@ -24,7 +24,7 @@ export const loadMenuLike = async () => {
 }
 
 //쿠키에서 id값 가져오는 함수
-export const getCookieValue = (key) => {
+export const getCookieValue = (key:string) => {
     const cookieKey = key + "=";
     let result = "";
     const cookieArr = document.cookie.split(";");
@@ -43,24 +43,24 @@ export const getCookieValue = (key) => {
 }
 
 //쿠키제거 함수
-export const deleteCookie = (cookieName) => {
+export const deleteCookie = (cookieName:string) => {
     document.cookie = cookieName + '=; expires=Thu, 01 Jan 1970 00:00:00 GMT; path=/';
 }
 
 //정규식으로 아이디 유효성 검사
-export const isValidId = (id) => {
+export const isValidId = (id:string) => {
     const regex = /^[a-zA-Z0-9]{1,10}$/;
     return regex.test(id);
 }
 
 //정규식으로 패스워드 유효성 검사
-export const isValidPassword = (password) => {
+export const isValidPassword = (password:string) => {
     const regex = /^.{1,15}$/;
     return regex.test(password);
 }
 
 //정규식으로 이메일 유효성 검사
-export const isValidEmail = (email) => {
+export const isValidEmail = (email:string) => {
     const regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;// eslint-disable-line no-useless-escape
     return regex.test(String(email).toLowerCase());
 }
