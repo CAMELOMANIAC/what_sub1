9b39542170a4d2ca6dbfe00a672d1b978b2c3a31
yugyusang <yugyusang12@gmail.com>
2024-02-19 23:09:56 +0900

    애니메이션 후크 추가

---
+++ utils/animationHook.ts
@@ -0,0 +1,19 @@
+import { RefObject, useEffect } from 'react';
+
+const useAnimation = (ref:RefObject<HTMLDivElement>, isValid:(id:unknown)=>boolean, id:unknown, animationName = 'vibration 0.1s infinite') => {
+    useEffect(() => {
+        if (!isValid(id) && ref.current) {
+            ref.current.style.animation = animationName;
+            const timeoutId = setTimeout(() => {
+                if (ref.current)
+                    ref.current.style.animation = '';
+            }, 300);
+
+            return () => {
+                clearTimeout(timeoutId);
+            };
+        }
+    }, [id]);
+}
+
+export default useAnimation;
