a53c905f70ff5a78ded718d71d74d806aa8c849f
yugyusang <yugyusang12@gmail.com>
2024-02-26 22:49:23 +0900

    aws로 db를 이전하면서 serverless-mysql대신 mysql2를 사용하도록 수정 및 그에따른 db통신 코드수정

--- lib/db.ts
+++ lib/db.ts
@@ -1,24 +1,21 @@
 // db.js
-import mysql from 'serverless-mysql';
+import mysql from 'mysql2/promise';
 
-const db = mysql({
-  config: {
-    host: process.env.MYSQL_HOST,
-    port: parseInt(process.env.MYSQL_PORT!),
-    database: process.env.MYSQL_DATABASE,
-    user: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASSWORD,
-    multipleStatements: true//기본적으로 sql인젝션 공격을 방지하기 위해 다중 문장은 지원하지 않는다.(다중 문장 지원을 위해서는 이 옵션을 켜야 한다.)
-  }
+const db = mysql.createConnection({
+  host: process.env.MYSQL_HOST,
+  port: (process.env.MYSQL_PORT as unknown as number),
+  database: process.env.MYSQL_DATABASE,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
 });
 
 //이곳에서 에러처리 하지 않고 에러처리는 비즈니스 로직에서 처리합니다
-const executeQuery = async<T>({ query, values }: { query: string, values: Array<string|number|Date> }): Promise<T|Error> => {
-  const results = await db.query<T>(query, values); // db.query 함수에도 제네릭을 적용합니다.
-  await db.end();
-  return results;
+const executeQuery = async<T>({ query, values }: { query: string, values: Array<string|number|Date> }): Promise<T | Error> => {
+  const connection = await db;
+  const [results] = await connection.query(query, values);
+  return results as T | Error;
 }
-
-
-
 export default executeQuery;
