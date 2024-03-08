6fff9f3fa60b6db5047adfad56227c447fb44356
yugyusang <yugyusang12@gmail.com>
2024-03-08 20:46:51 +0900

    db와 통신할때 배열도 사용할수있도록 수정

--- lib/db.ts
+++ lib/db.ts
@@ -13,7 +13,7 @@ const db = mysql.createConnection({
 });
 
 //이곳에서 에러처리 하지 않고 에러처리는 비즈니스 로직에서 처리합니다
-const executeQuery = async<T>({ query, values }: { query: string, values: Array<string|number|Date> }): Promise<T | Error> => {
+const executeQuery = async<T>({ query, values }: { query: string, values: Array<string|string[]|number|number[]|Date> }): Promise<T | Error> => {
   const connection = await db;
   const [results] = await connection.query(query, values);
   return results as T | Error;
