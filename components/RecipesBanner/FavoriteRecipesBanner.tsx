57cb856919eb989467f29061297ddb9409fdeb41
yugyusang <yugyusang12@gmail.com>
2024-03-18 23:58:48 +0900

    FavoriteRecipesBanner 구현

--- components/RecipesBanner/FavoriteRecipesBanner.tsx
+++ components/RecipesBanner/FavoriteRecipesBanner.tsx
@@ -1,74 +1,102 @@
 import { useRouter } from 'next/router';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { IoIosArrowBack } from 'react-icons/io';
 import { useSelector } from 'react-redux';
 import { RootState } from '../../redux/store';
-import { useQuery } from 'react-query';
+import useMenuLike from '../../utils/menuLikeHook';
+import { PiHeartStraightFill, PiHeartStraight } from 'react-icons/pi';
+import { MdOutlineArrowForward } from "react-icons/md";
+import Link from 'next/link';
 
 const WriteRecipesBanner = ({ref}) => {
     const router = useRouter();
     const user = useSelector((state: RootState) => state.user);
-    const [menuLikeArrayL, setMenuLikeArrayL] = useState<Array<{sandwich_table_sandwich_name:string}>>([]);
-    const [menuLikeArrayR, setMenuLikeArrayR] = useState<Array<{sandwich_table_sandwich_name:string}>>([]);
-    useQuery('userMenuLike', async () => {
-        const response = await fetch('/api/users/menus/like');
-        if (response.ok) {
-            return response.json();
-        } else {
-            throw new Error('실패')
-        }
-    },{
-        onSuccess: (data) => {
-            const left = data.slice(0, data.length / 2);
-            const right = data.slice(data.length / 2, data.length);
-            setMenuLikeArrayL(left);
-            setMenuLikeArrayR(right);
-        }
-    });
-    
+    const [menuLikeArrayL, setMenuLikeArrayL] = useState<Array<string>>([]);
+    const [menuLikeArrayR, setMenuLikeArrayR] = useState<Array<string>>([]);
+
+    const menuLikeArray = useSelector((state: RootState) => state.user.menuLikeArray);
+    useEffect(() => {
+        const left = menuLikeArray.slice(0, menuLikeArray.length / 2);
+        const right = menuLikeArray.slice(menuLikeArray.length / 2, menuLikeArray.length);
+        setMenuLikeArrayL(left);
+        setMenuLikeArrayR(right);
+    }, [menuLikeArray]);
+
+    const recipeLikeCount = useSelector((state: RootState) => state.user.menuLikeArray.length);
+    const menuLikeCount = useSelector((state: RootState) => state.user.recipeLikeArray.length);
+    const MenuLikeButton = ({menuName}:{menuName:string}) => {
+        const {menuLikeHandler} = useMenuLike(menuName);
+        const [hover, setHover] = useState<boolean>(false);
+        return (
+            <button className='flex items-center text-xl text-green-600' onMouseEnter={()=>setHover(true)} onMouseLeave={()=>setHover(false)} onClick={() => menuLikeHandler()}>
+                {!hover ? <PiHeartStraightFill className='inline-block' /> : <PiHeartStraight className='inline-block' />}
+            </button>
+        )
+    };
+
     return (
         <section className={`relative flex justify-center w-screen right-0 bg-white border-gray-200 border-b min-w-[640px]`} ref={ref}>
             <button className='py-10 my-auto h-full bg-gray-100 hover:text-green-600'
                 onClick={router.back}>
                 <IoIosArrowBack className='inline text-lg h-1/2' />
             </button>
-            <section className="flex flex-col justify-start pt-4 pb-10 w-full max-w-[1024px]">
+            <section className="flex flex-col justify-start pt-4 md:pb-10 w-full max-w-[1024px]">
                 <article className='flex flex-col pb-5 pl-4 border-l'>
-                    <h1 className='font-bold text-3xl inline text-black pb-4'>{user.userName}님이 좋아하는 레시피</h1>
+                    <h1 className='font-bold text-3xl inline text-black pb-4'>{user.userName}님이 남긴 좋아요</h1>
                     <div className='whitespace-pre-line'>
                         <div className='flex flex-row text-sm m-2'>
                             <div className='w-40 px-3'>
-                                <div className='text-gray-500'>좋아요한 갯수</div>
-                                <div className='font-bold'>{0}</div>
+                                <div className='text-gray-500'>레시피 좋아요 갯수</div>
+                                <div className='font-bold'>{recipeLikeCount}</div>
+                            </div>
+                            <div className='border-l w-40 px-3'>
+                                <div className='text-gray-500'>메뉴 좋아요 갯수</div>
+                                <div className='font-bold'>{menuLikeCount}</div>
                             </div>
                         </div>
                     </div>
                 </article>
                 
                 <section className='grid grid-cols-1 md:grid-cols-2 grid-flow-row'>
-                    <article className='md:border-l md:border-b-0 md:mb-0 px-4 col-span-1 border-b border-l-0 mb-3'>
+                    <article className='md:border-l md:mb-0 px-4 col-span-1 border-l-0'>
                         <span className=' font-bold'>메뉴 좋아요</span>
                         <div className='text-sm text-gray-500 grid grid-cols-10 grid-flow-row text-center'>
                             <span className='col-span-5 text-left'>메뉴이름</span>
-                            <span className='col-span-2'>조합 선택율</span>
-                            <span className='col-span-2'>좋아요 수</span>
+                            <span className='col-span-2'>좋아요</span>
+                            <span className='col-span-2'>보러가기</span>
                         </div>
                         {Array.isArray(menuLikeArrayL) && menuLikeArrayL.map((item, index) => (
-                            <div key={index} className='font-normal text-gray-500 grid grid-cols-7 grid-flow-row h-10 my-2'>
+                            <div key={index} className='font-normal text-gray-500 grid grid-cols-10 grid-flow-row h-10 my-2'>
                                 <span className='col-span-5 flex items-center justify-start text-black font-bold'>
-                                    {' ' + item.sandwich_table_sandwich_name}
+                                    {' ' + item}
                                 </span>
-                                <span className='col-span-2 flex items-center justify-center text-sm text-black'>{}</span>
+                                <span className='col-span-2 flex items-center justify-center text-sm text-black'>
+                                    {<MenuLikeButton menuName={item}/>}
+                                </span>
+                                <span className='col-span-2 flex items-center justify-center text-sm text-black'>
+                                    <Link href={`/Recipes?param=${item}`}><MdOutlineArrowForward/></Link>
+                                </span>
                             </div>
                         ))}
                     </article>
-                    <article className='md:border-l md:border-b-0 md:mb-0 px-4 col-span-1 border-b border-l-0 mb-3'>
+                    <article className='md:border-l md:border-b-0 px-4 col-span-1 border-b border-l-0 md:mb-3'>
+                        <span className='invisible font-bold hidden md:block'>메뉴 좋아요</span>
+                        <div className='text-sm text-gray-500 md:grid grid-cols-10 grid-flow-row text-center hidden'>
+                            <span className='col-span-5 text-left'>메뉴이름</span>
+                            <span className='col-span-2'>좋아요</span>
+                            <span className='col-span-2'>보러가기</span>
+                        </div>
                         {Array.isArray(menuLikeArrayR) && menuLikeArrayR.map((item, index) => (
-                            <div key={index} className='font-normal text-gray-500 grid grid-cols-7 grid-flow-row h-10 my-2'>
+                            <div key={index} className='font-normal text-gray-500 grid grid-cols-10 grid-flow-row h-10 my-2'>
                                 <span className='col-span-5 flex items-center justify-start text-black font-bold'>
-                                    {' ' + item.sandwich_table_sandwich_name}
+                                    {' ' + item}
                                 </span>
-                                <span className='col-span-2 flex items-center justify-center text-sm text-black'>{}</span>
+                                <span className='col-span-2 flex items-center justify-center text-sm text-black'>
+                                    <MenuLikeButton menuName={item}/>
+                                </span>
+                                <span className='col-span-2 flex items-center justify-center text-sm text-black'>
+                                    <Link href={`/Recipes?param=${item}`}><MdOutlineArrowForward/></Link>
+                                </span>
                             </div>
                         ))}
                     </article>
