f8620c4df0be4b42c39fd211cfe59c6cbd930310
yugyusang <yugyusang12@gmail.com>
2024-06-11 00:35:01 +0900

    notoSans 로컬 폰트를 next/font로 최적화

--- next.config.js
+++ next.config.js
@@ -1,3 +1,8 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
+});
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
@@ -5,13 +10,14 @@ const nextConfig = {
     styledComponents: true,
   },
   env: {
-    'URL': "http://localhost:3000",
-    KAKAO_CLIENT_SECRET:"aEuOy5Dj6HILcuDDfnyRXxbvzm1gMdWL",
-    KAKAO_RESTAPI_KEY:"f417de962e4f7161ae761a86c4653cd2"
+    URL: "http://localhost:3000",
+    KAKAO_CLIENT_SECRET: "aEuOy5Dj6HILcuDDfnyRXxbvzm1gMdWL",
+    KAKAO_RESTAPI_KEY: "f417de962e4f7161ae761a86c4653cd2",
   },
   i18n: {
-    locales: ['ko'],
-    defaultLocale: 'ko',
+    locales: ["ko"],
+    defaultLocale: "ko",
   },
 };
-module.exports = nextConfig
+
+module.exports = withBundleAnalyzer(nextConfig);
